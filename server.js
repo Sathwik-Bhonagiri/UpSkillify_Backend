c7192b901970a8066147985daeaff979fbb0aac5
Sathwik-Bhonagiri <148807374+Sathwik-Bhonagiri@users.noreply.github.com>
2025-06-14 18:15:40 +0530

    Added Changes

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.post('/stripe',express.raw({type: 'application/json'}), stripeWebhooks)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
